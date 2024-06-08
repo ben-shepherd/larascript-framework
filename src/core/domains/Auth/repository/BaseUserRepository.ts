462cf4abfce8cd594192b218f55b78121ee37601
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-06-08 16:12:01 +0100

    Fix incorrect type returned in BaseUserRepository

--- src/core/domains/Auth/repository/BaseUserRepository.ts
+++ src/core/domains/Auth/repository/BaseUserRepository.ts
@@ -3,13 +3,15 @@ import { IRepository } from '../../../interfaces/IRepository';
 import BaseUserModel from '../models/BaseUserModel';
 import { BaseUserData } from '../types/types.t';
 
-export default class BaseUserRepositor<M extends BaseUserModel = BaseUserModel, D extends BaseUserData = BaseUserData> extends Repository<BaseUserModel> implements IRepository {
+type Constructor<M,D> = new (data: D) => M
 
-    constructor(model: new (data: D) => M = BaseUserModel as new (data: D) => M) {
+export default class BaseUserRepository<M extends BaseUserModel = BaseUserModel, D extends BaseUserData = BaseUserData> extends Repository<M> implements IRepository {
+
+    constructor(model: Constructor<M,D> = BaseUserModel as Constructor<M,D>) {
         super('users', model);
     }
 
     async findByEmail(email: string): Promise<M | null> {
-        return await this.find({ email }) as M
+        return await this.findOne({ email }) as M
     }
 }
