258690a0e9cea7b1052fc68d2da77cd674bb03ea
Benjamin <benjamin.shepherd@shift.online>
2024-06-16 23:30:23 +0100

    remove tests

--- src/app/events/listeners/ExampleListener.ts
+++ src/app/events/listeners/ExampleListener.ts
@@ -1,12 +1,8 @@
 import EventListener from "@src/core/domains/events/services/EventListener";
  
 export class ExampleListener extends EventListener<{userId: string}> {
-
+    
     handle = async (payload: { userId: string}) => {
-        console.log('[ExampleListener]', payload.userId, payload.userId === '666da65fe9e83efba0f9ab89' ? 'will throw' : 'ok')
-
-        if(payload.userId === '666da65fe9e83efba0f9ab89') {
-            throw new Error('something went wrong!')
-        }
+        console.log('[ExampleListener]', payload.userId)
     }
 }
