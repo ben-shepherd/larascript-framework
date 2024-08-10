adc600d848f8f35778e53714c69f2dd01c3456ec
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-08-11 00:09:54 +0100

    Removed testing files

--- src/app/observers/UserObserver.ts
+++ src/app/observers/UserObserver.ts
@@ -3,12 +3,5 @@ import { UserData } from "../models/auth/User";
 
 export default class UserObserver extends Observer<UserData>
 {  
-    updating = (data: UserData): UserData => {
-        return data
-    }
 
-    onPasswordChanged = (data: UserData): UserData => {
-        console.log(`[UserObserver] ${data.email} changed their password`)
-        return data
-    }
 }
