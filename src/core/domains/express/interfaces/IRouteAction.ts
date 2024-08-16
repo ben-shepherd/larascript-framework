a0c0293a27e0d7306c5278b8ccb75b45b8fd7181
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-08-16 20:48:41 +0100

    Fix bad import, remove accidental added test file

--- src/core/domains/express/interfaces/IRouteAction.ts
+++ src/core/domains/express/interfaces/IRouteAction.ts
@@ -1,4 +1,4 @@
-import { BaseRequest } from "@src/core/domains/express/types/BaseRequest";
+import { BaseRequest } from "@src/core/domains/express/types/BaseRequest.t";
 import { Response } from "express";
 
 export type IRouteAction = (req: BaseRequest, res: Response) => any;
