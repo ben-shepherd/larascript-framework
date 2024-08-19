437c1fa785a487464f8106c7648543a5cdfb3014
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-08-19 15:02:06 +0100

    Renamed containers interface, moved to core interfaces, added example AppService

---
+++ src/app/services/AppService.ts
@@ -0,0 +1,12 @@
+import Service from "@src/core/base/Service";
+import { IAppService } from "../interfaces/IAppService";
+
+class AppService extends Service implements IAppService
+{
+    /**
+     * Add your own app methods
+     * Access wth App.container('app')
+     */
+}
+
+export default AppService
--- src/config/containers.ts
+++ src/config/containers.ts
@@ -1,54 +1,9 @@
-import { IAuthService } from '@src/core/domains/auth/interfaces/IAuthService';
-import ICommandService from '@src/core/domains/console/interfaces/ICommandService';
-import { IMongoDB } from '@src/core/domains/database/mongodb/interfaces/IMongoDB';
-import { IEventService } from '@src/core/domains/events/interfaces/IEventService';
-import IExpress from '@src/core/domains/express/interfaces/IExpress';
-import IValidatorService from '@src/core/domains/validator/interfaces/IValidatorService';
+import { IAppService } from "@src/app/interfaces/IAppService";
+import { ICoreContainers } from "@src/core/interfaces/ICoreContainers";
 
-/**
- * ContainersTypeHelpers provides type hinting when accessing a container. 
- *
- * [How to set a container]
- *   In your provider, set a container by utilising the Kernel.setContainer(name: string, container: any) method
- *   Example:
- *   Kernel.setContainer('auth', new AppAuthService())
- *
- * [Retrieving a container]
- *   Retrieving the container using App global helper
- *   Example:
- *   const token = await App.container('auth').createToken(user)
- */
-
-export interface ContainersTypeHelpers {
-    [key: string]: any;
+export interface IContainers extends ICoreContainers {
     /**
-     * Event Dispatcher Service
-     * Provided by '@src/core/providers/EventProvider'
+     * Add interfaces to your container services here
      */
-    events: IEventService;
-    /**
-     * Auth service
-     * Provided by '@src/core/providers/AuthProvider'
-     */
-    auth: IAuthService;
-    /**
-     * MongoDB Service
-     * Provided by '@src/core/providers/MongoDBProvider'
-     */
-    mongodb: IMongoDB;
-    /**
-     * Express web server Service
-     * Provided by '@src/core/providers/ExpressProvider'
-     */
-    express: IExpress;
-    /**
-     * Console service
-     * Provided by '@src/core/domains/console/providers/ConsoleProvider'
-     */
-    console: ICommandService;
-    /**
-     * Validator service
-     * Provided by '@src/core/domains/validator/providers/ValidatorProvider'
-     */
-    validate: IValidatorService;
+    app: IAppService;
 }
