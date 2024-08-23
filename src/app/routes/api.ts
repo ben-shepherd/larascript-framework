d91469121a43348062a86a734d76fe7c495faca3
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-08-23 22:59:53 +0100

    revert from testing changes

--- src/app/routes/api.ts
+++ src/app/routes/api.ts
@@ -1,10 +1,6 @@
 import Route from "@src/core/domains/express/routing/Route"
 import RouteGroup from "@src/core/domains/express/routing/RouteGroup"
-import RouteResource from "@src/core/domains/express/routing/RouteResource"
 import { Request, Response } from "express"
-import User from "@src/app/models/auth/User"
-import CreateUserValidator from "@src/app/validators/user/CreateUserValidator"
-import UpdateUserValidator from "@src/app/validators/user/UpdateUserValidator"
 
 const routes = RouteGroup([
     Route({
@@ -12,16 +8,8 @@ const routes = RouteGroup([
         method: 'get',
         path: '/',
         action: (req: Request, res: Response) => {
-            res.send('Hello world!')
+            res.send('OK!')
         }
-    }),
-    ...RouteResource({
-        resource: User,
-        name: 'users',
-        except: ['index'],
-        only: ['show'],
-        createValidator: CreateUserValidator,
-        updateValidator: UpdateUserValidator
     })
 ])
 
