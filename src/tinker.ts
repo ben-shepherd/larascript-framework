3c6db6eaac2cd21634d105697bd107dde53968cb
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-09-07 21:47:06 +0100

    Updated Tinker

--- src/tinker.ts
+++ src/tinker.ts
@@ -3,17 +3,25 @@ import 'tsconfig-paths/register';
 
 import appConfig from '@src/config/app';
 import Kernel from "@src/core/Kernel";
-import { App } from '@src/core/services/App';
+import TestDatabaseProvider from './tests/providers/TestDatabaseProvider';
 
 (async () => {
-    await Kernel.boot(appConfig, {})
+    await Kernel.boot({
+        ...appConfig,
+        providers: [
+            new TestDatabaseProvider(),
+            ...appConfig.providers,
+        ]
+    }, {
+        withoutProvider: ['DatabaseProvider']
+    })
 
-    const auth = App.container('auth');
-    const db = App.container('db');
-    const events = App.container('events')
-    const express = App.container('express')
-    const cnsl = App.container('console');
-    const validator = App.container('validate')
+    // const auth = App.container('auth');
+    // const db = App.container('db');
+    // const events = App.container('events')
+    // const express = App.container('express')
+    // const cnsl = App.container('console');
+    // const validator = App.container('validate')
 
     // add your tinkers below
 
