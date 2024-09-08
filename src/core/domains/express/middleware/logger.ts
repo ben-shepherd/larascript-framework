038df59dbc58950a174328f381d39e3c98b567c4
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-09-09 00:47:48 +0100

    logger middleware

--- src/core/domains/express/middleware/logger.ts
+++ src/core/domains/express/middleware/logger.ts
@@ -1,6 +1,7 @@
-import { NextFunction, Request, Response } from 'express';
+import { BaseRequest } from '@src/core/domains/express/types/BaseRequest.t';
+import { NextFunction, Response } from 'express';
 
-export const logger = (req: Request, res: Response, next: NextFunction) => {
-    console.log('LOGGER', `${req.method} ${req.url}`);
+export const logger = () => async (req: BaseRequest, res: Response, next: NextFunction): Promise<void> => {
+    console.log('Request', `${req.method} ${req.url}`);
     next();
 };
