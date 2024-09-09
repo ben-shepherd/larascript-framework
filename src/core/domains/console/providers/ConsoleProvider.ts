7ec2cab48bd742599e282b4c40805ecf79c3ad68
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-09-09 23:51:38 +0100

    Changed commands config to src/config/commands

--- src/core/domains/console/providers/ConsoleProvider.ts
+++ src/core/domains/console/providers/ConsoleProvider.ts
@@ -1,12 +1,17 @@
-import appConfig from "@src/config/app";
+import commandsConfig from "@src/config/commands";
 import BaseProvider from "@src/core/base/Provider";
 import ConsoleService from "@src/core/domains/console/service/ConsoleService";
 import { App } from "@src/core/services/App";
 
 export default class ConsoleProvider extends BaseProvider {
 
+    /**
+     * Register method
+     * Called when the provider is being registered
+     * Use this method to set up any initial configurations or services
+     */
     async register(): Promise<void> {
-        this.log('Registering ConsoleProvider')
+        this.log('Registering ConsoleProvider');
 
         /**
          * Add the console service to the container
@@ -16,9 +21,14 @@ export default class ConsoleProvider extends BaseProvider {
         /**
          * Register commands from @src/config/app
          */
-        App.container('console').register().registerAll(appConfig.commands)
+        App.container('console').register().registerAll(commandsConfig)
     }
 
+    /**
+     * Boot method
+     * Called after all providers have been registered
+     * Use this method to perform any actions that require other services to be available
+     */
     async boot(): Promise<void> {}
 
 }
--- src/core/interfaces/IAppConfig.ts
+++ src/core/interfaces/IAppConfig.ts
@@ -1,9 +1,7 @@
 import { EnvironmentType } from "@src/core/consts/Environment";
-import { ICommandConstructor } from "@src/core/domains/console/interfaces/ICommand";
 import { IProvider } from "@src/core/interfaces/IProvider";
 
 export default interface IAppConfig {
     environment: EnvironmentType
     providers: IProvider[]
-    commands: ICommandConstructor[]
 }
