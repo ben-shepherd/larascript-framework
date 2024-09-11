7bbb1cdbde3bb8cf04c80e5eb80f53aaf581a155
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-09-11 02:17:28 +0100

    Updated comments on validator domain

--- src/core/domains/validator/base/Validator.ts
+++ src/core/domains/validator/base/Validator.ts
@@ -1,6 +1,11 @@
-import Joi from "joi";
 import BaseValidator from "@src/core/domains/validator/base/BaseValidator";
+import Joi from "joi";
 
+/**
+ * A class that extends the BaseValidator. This class is used to validate objects.
+ *
+ * The rules() method must be implemented and return a Joi ObjectSchema.
+ */
 class Validator extends BaseValidator {
 
     constructor(rules: Joi.ObjectSchema = Joi.object({})) {
--- src/core/domains/validator/middleware/validateMiddleware.ts
+++ src/core/domains/validator/middleware/validateMiddleware.ts
@@ -1,8 +1,18 @@
-import { NextFunction, Response } from 'express';
 import responseError from '@src/core/domains/express/requests/responseError';
 import { BaseRequest } from '@src/core/domains/express/types/BaseRequest.t';
 import { ValidatorMiddlewareProps } from '@src/core/domains/validator/interfaces/IValidatorService';
+import { NextFunction, Response } from 'express';
 
+/**
+ * A middleware that validates the request body using the provided validator.
+ * If validateBeforeAction is true, it will validate the request body before
+ * calling the next middleware. If the validation fails, it will send a 400
+ * response with the validation errors. If the validation succeeds, it will
+ * call the next middleware.
+ *
+ * @param {{validator: IValidator, validateBeforeAction: boolean}} options
+ * @returns {import('express').RequestHandler} The middleware function
+ */
 export const validateMiddleware = ({validator, validateBeforeAction}: ValidatorMiddlewareProps) => async (req: BaseRequest, res: Response, next: NextFunction): Promise<void> => {
     try {
         req.validator = validator;
