e846373b6b78692829db72088f92438b0bc2fec0
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-09-14 23:20:31 +0100

    Updated readme

--- src/app/routes/api.ts
+++ src/app/routes/api.ts
@@ -1,8 +1,11 @@
 import Route from "@src/core/domains/express/routing/Route"
 import RouteGroup from "@src/core/domains/express/routing/RouteGroup"
+import RouteResource from "@src/core/domains/express/routing/RouteResource"
 import { Request, Response } from "express"
 
 const apiRoutes = RouteGroup([
+
+    // Single route
     Route({
         name: 'index',
         method: 'get',
@@ -10,6 +13,15 @@ const apiRoutes = RouteGroup([
         action: (req: Request, res: Response) => {
             res.send('OK!')
         }
+    }),
+
+    // Auto generated routes
+    ...RouteResource({
+        name: 'blog',
+        resource: BlogModel,
+        createValidator: BlogCreateValidator,
+        updateValidator: BlogUpdateValidator,
+        except: ['destory'],
     })
 ])
 
