ee9023087ce0273ed13ea2a05914fa03086a26ca
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-09-15 23:48:11 +0100

    Updated tinker

--- src/tinker.ts
+++ src/tinker.ts
@@ -3,21 +3,11 @@ import 'tsconfig-paths/register';
 
 import appConfig from '@src/config/app';
 import Kernel from "@src/core/Kernel";
-import TestDatabaseProvider from '@src/tests/providers/TestDatabaseProvider';
-import Joi from 'joi';
 
-import { App } from './core/services/App';
 
 (async () => {
-    await Kernel.boot({
-        ...appConfig,
-        providers: [
-            new TestDatabaseProvider(),
-            ...appConfig.providers,
-        ]
-    }, {
-        withoutProvider: ['DatabaseProvider']
-    })
+
+    await Kernel.boot(appConfig, {})
 
     // const auth = App.container('auth');
     // const db = App.container('db');
@@ -25,21 +15,7 @@ import { App } from './core/services/App';
     // const express = App.container('express')                         
     // const cnsl = App.container('console');
 
-    // Get the validation service
-    const validatorService = App.container('validate');
+    // Add your tinkers below
 
-    // Create a validator
-    const validator = validatorService.validator(
-        Joi.object({
-            email: Joi.string().email().required()
-        })
-    );
-
-    // Validate the payload
-    const result = await validator.validate({
-        email: 'badEmail@examplecom',
-    })
-
-    // Print the result
-    console.log(result.success, result.joi.error)
+    
 })(); 
