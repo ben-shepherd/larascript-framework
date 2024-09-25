9ae448c4d54fb9bff9badab28044398011217e2c
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-09-25 16:12:40 +0100

    Added todo comment

--- src/core/domains/express/interfaces/ISecurity.ts
+++ src/core/domains/express/interfaces/ISecurity.ts
@@ -22,6 +22,7 @@ export type IIdentifiableSecurityCallback = {
     // The identifier for the security callback.
     id: string;
     // Include another security rule in the callback.
+    //  TODO: We could add another type here 'alsoArguments' if extra parameters are required
     also?: string;
     // The condition for when the security check should be executed. Defaults to 'always'.
     when: string[] | null;
