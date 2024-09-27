677f2eafa28b0cf68ba4a04c08f20953f20c46ed
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-09-27 23:10:05 +0100

    Updated comment

--- src/core/domains/express/rules/hasScopeSecurity.ts
+++ src/core/domains/express/rules/hasScopeSecurity.ts
@@ -4,7 +4,8 @@ import { BaseRequest } from "@src/core/domains/express/types/BaseRequest.t";
  * Checks if the given scope(s) are present in the scopes of the current request's API token.
  * If no API token is found, it will return false.
  * @param req The request object
- * @param scopesExactMatch The scope(s) to check
+ * @param scopesExactMatch The scope(s) to check - must be an exact match - ignores empty scopes
+ * @param scopesPartialMatch The scope(s) to check - must be a partial match - ignores empty scopes
  * @returns True if all scopes are present, false otherwise
  */
 const hasScopeSecurity = (req: BaseRequest, scopesExactMatch: string | string[], scopesPartialMatch: string | string[]): boolean => {
