15b62fe99fb71e9272a847ddb4ea6228e7427f1e
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-11-06 21:17:34 +0000

    removed unused interface

--- src/core/domains/events/interfaces/IEventPayload.ts
+++ src/core/domains/events/interfaces/IEventPayload.ts
@@ -1,8 +1,3 @@
 export type TSerializableValues = number | string | boolean | undefined | null;
 
 export type TISerializablePayload = Record<string | number | symbol, unknown> | TSerializableValues;
-
-export interface IEventPayloadWithDriver {
-    driver: string;
-    payload: TISerializablePayload
-}
