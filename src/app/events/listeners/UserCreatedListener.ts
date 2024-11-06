4a41316fb0113a5229b1ed444e5363f9a5528a92
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-11-06 20:52:53 +0000

    Updated make for subscribers, listeners

--- src/app/events/listeners/UserCreatedListener.ts
+++ src/app/events/listeners/UserCreatedListener.ts
@@ -1,15 +1,15 @@
-import { IUserData } from "@src/app/models/auth/User";
 import BaseEventListener from "@src/core/domains/events/base/BaseEventListener";
  
 export class UserCreatedListener extends BaseEventListener {
 
-     
+    /**
+     * Optional method to execute before the subscribers are dispatched.
+     */
     async execute(): Promise<void> {
 
-        // eslint-disable-next-line no-unused-vars
-        const userData = this.getPayload<IUserData>();
+        // const payload = this.getPayload<IUserData>();
 
-        // Handle some logic
+        // Handle logic
     }
 
 }
