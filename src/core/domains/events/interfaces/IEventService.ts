d69c76a39556f0fa33f7c3fe6e376e2500c6ce82
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-11-06 21:20:56 +0000

    removed useless interfaces

--- src/core/domains/events/interfaces/IEventService.ts
+++ src/core/domains/events/interfaces/IEventService.ts
@@ -5,13 +5,12 @@ import { TListenersConfigOption } from "@src/core/domains/events/interfaces/conf
 import { IBaseEvent } from "@src/core/domains/events/interfaces/IBaseEvent";
 import IEventDriver from "@src/core/domains/events/interfaces/IEventDriver";
 import { IEventWorkerConcern } from "@src/core/domains/events/interfaces/IEventWorkerConcern";
-import { IHasDispatcherConcern } from "@src/core/domains/events/interfaces/IHasDispatcherConcern";
-import { IHasListenerConcern } from "@src/core/domains/events/interfaces/IHasListenerConcern";
 import { IMockableConcern } from "@src/core/domains/events/interfaces/IMockableConcern";
+import { IDispatchable } from "@src/core/interfaces/concerns/IDispatchable";
 import { IHasRegisterableConcern } from "@src/core/interfaces/concerns/IHasRegisterableConcern";
 import { ICtor } from "@src/core/interfaces/ICtor";
 
-export interface IEventService extends IHasRegisterableConcern, IHasDispatcherConcern, IHasListenerConcern, IEventWorkerConcern, IMockableConcern
+export interface IEventService extends IHasRegisterableConcern, IDispatchable, IEventWorkerConcern, IMockableConcern
 {
     getConfig(): IEventConfig;
 
--- src/core/domains/events/interfaces/IHasDispatcherConcern.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-unused-vars */
-import { IBaseEvent } from "@src/core/domains/events/interfaces/IBaseEvent";
-
-export interface IHasDispatcherConcern {
-
-    dispatch: (event: IBaseEvent) => Promise<void>;
-}
