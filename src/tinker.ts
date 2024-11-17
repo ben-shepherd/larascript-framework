404fc2c843752f5137fe016952508878238c93b6
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-11-17 18:50:14 +0000

    revert tinker

--- src/tinker.ts
+++ src/tinker.ts
@@ -1,34 +1,22 @@
 import 'dotenv/config';
 import 'tsconfig-paths/register';
 
+import appConfig from '@src/config/app';
+import Kernel from "@src/core/Kernel";
 import { App } from '@src/core/services/App';
-import testHelper from '@src/tests/testHelper';
 
-import BaseRegister from './core/base/BaseRegister';
-import DatabaseAdapter from './core/domains/database/services/DatabaseAdapter';
-
-class TinkerClass extends BaseRegister {
-
-    static readonly COUNTRIES_AND_CITIES = 'countriesAndCities';
-
-}
 
 (async () => {
 
-    await testHelper.testBootApp()
+    await Kernel.boot(appConfig, {})
 
     // const auth = App.container('auth');
-    const db = App.container('db');
+    // const db = App.container('db');
     // const events = App.container('events')
     // const express = App.container('express')                         
     // const cnsl = App.container('console');
 
+    // Add your tinkers below
 
-    
-
-    console.log('adapters', db.getAllAdapterConstructors())
-
-    console.log(
-        DatabaseAdapter.getComposerShortFileNames()
-    )
+    App.container('logger').info('Tinkers are ready!')
 })(); 
