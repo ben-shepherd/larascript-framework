5042bce674aad427815ca6c610fd77db567e4770
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-11-17 18:43:16 +0000

    added comments to DatabaseConfig

--- src/core/domains/database/services/DatabaseConfig.ts
+++ src/core/domains/database/services/DatabaseConfig.ts
@@ -5,10 +5,23 @@ import { IDatabaseConfig, IDatabaseGenericConnectionConfig } from "@src/core/dom
 
 class DatabaseConfig {
 
+    /**
+     * Creates a connections array for the database config.
+     * @param connectionsConfigArray The array of connection configurations
+     * @returns The connections array
+     */
     public static createConnections(connectionsConfigArray: IDatabaseGenericConnectionConfig[]): IDatabaseConfig['connections'] {
         return connectionsConfigArray;
     }
 
+    /**
+     * Creates a database configuration object from a connection configuration object.
+     * 
+     * This method simply returns the given connection configuration object as is, without performing any changes.
+     * 
+     * @param config - The connection configuration object implementing IDatabaseGenericConnectionConfig interface.
+     * @returns The given connection configuration object as an IDatabaseGenericConnectionConfig.
+     */
     public static createConfig(config: IDatabaseGenericConnectionConfig): IDatabaseGenericConnectionConfig {
         return config
     }
