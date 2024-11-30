c8a6e09ebd47676dcbf60b69ff69420603b76f31
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-11-30 18:22:29 +0000

    eloquent insert test

---
+++ src/tests/eloquent/eloquentInsert.test.ts
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+import { describe } from '@jest/globals';
+import testHelper from '@src/tests/testHelper';
+
+import TestPeopleModel, { resetTable } from './models/TestPeopleModel';
+
+describe('eloquent', () => {
+
+    beforeAll(async () => {
+        await testHelper.testBootApp()
+        await resetTable()
+    });
+
+    test('test insert records', async () => {
+        const results = await TestPeopleModel.query().insert([
+            {
+                name: 'John',
+                age: 25,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                name: 'Jane',
+                age: 30,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+        ]);
+
+        expect(results.count()).toBe(2);
+
+        expect(typeof results.get(0)?.id === 'string').toBeTruthy()
+        expect(results.get(0)?.name).toBe('John')
+        expect(results.get(0)?.age).toBe(25)
+
+        expect(typeof results.get(1)?.id === 'string').toBeTruthy()
+        expect(results.get(1)?.name).toBe('Jane')
+        expect(results.get(1)?.age).toBe(30)
+
+    });
+});
---
+++ src/tests/eloquent/models/TestPeopleModel.ts
@@ -0,0 +1,46 @@
+import Model from "@src/core/base/Model";
+import IModelAttributes from "@src/core/interfaces/IModelData";
+import { App } from "@src/core/services/App";
+import testHelper from "@src/tests/testHelper";
+import { DataTypes } from "sequelize";
+
+const tableName = Model.formatTableName('testsPeople')
+
+export interface ITestPeopleModelData extends IModelAttributes {
+    id: string,
+    name: string;
+    age: number;
+    createdAt: Date;
+    updatedAt: Date;
+
+}
+
+export const resetTable = async (connections: string[] = testHelper.getTestConnectionNames()) => {
+    for(const connectionName of connections) {
+        const schema = App.container('db').schema(connectionName);
+
+        if(await schema.tableExists(tableName)) {
+            await schema.dropTable(tableName);
+        }
+
+        await schema.createTable(tableName, {
+            name: DataTypes.STRING,
+            age: DataTypes.INTEGER,
+            createdAt: DataTypes.DATE,
+            updatedAt: DataTypes.DATE
+        })
+    }
+}
+
+export default class TestPeopleModel extends Model<ITestPeopleModelData> {
+
+    table = tableName
+    
+    public fields: string[] = [
+        'name',
+        'age',
+        'createdAt',
+        'updatedAt'
+    ];
+
+}
