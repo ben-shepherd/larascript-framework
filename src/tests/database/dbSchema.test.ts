8ef1ea68d1451a15ee7805c7227a584860d9b69b
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-12-15 18:18:36 +0000

    dbSchema test fix

--- src/tests/database/dbSchema.test.ts
+++ src/tests/database/dbSchema.test.ts
@@ -1,6 +1,10 @@
  
 import { beforeAll, describe, expect, test } from '@jest/globals';
+import { db } from '@src/core/domains/database/services/Database';
+import { queryBuilder } from '@src/core/domains/eloquent/services/EloquentQueryBuilderService';
 import PostgresSchema from '@src/core/domains/postgres/PostgresSchema';
+import IModelAttributes from '@src/core/interfaces/IModelData';
+import Model from '@src/core/models/base/Model';
 import { App } from '@src/core/services/App';
 import testHelper from '@src/tests/testHelper';
 import { DataTypes } from 'sequelize';
@@ -8,61 +12,68 @@ import { DataTypes } from 'sequelize';
 
 const connections = testHelper.getTestConnectionNames()
 
-const tableName = 'testTable';
-type Data = { id?: string, name: string, age?: number };
+interface TestSchemaModelAttributes extends IModelAttributes { name: string, age?: number };
 
-const createTable = async (connectionName: string) => {
-    const schema = App.container('db').schema(connectionName);
-    await schema.createTable(tableName, {
-        name: DataTypes.STRING,
-    });
-};
+class TestSchemaModel extends Model<TestSchemaModelAttributes> {
 
-const dropTable = async (connectionName: string) => {
-    try {
-        const schema = App.container('db').schema(connectionName);
-        await schema.dropTable(tableName);
+    public table: string = 'tests'
+
+    public fields: string[] = ['name', 'age']
+
+}
+
+const resetTable = async () => {
+    for(const connectionName of testHelper.getTestConnectionNames()) {
+        try {
+            const schema = db().schema(connectionName);
+            await schema.dropTable(tableName);
+        }
+        // eslint-disable-next-line no-unused-vars
+        catch (err) {}
+
+        const schema = db().schema(connectionName);
+        await schema.createTable(tableName, {
+            name: DataTypes.STRING,
+        });
     }
-    // eslint-disable-next-line no-unused-vars
-    catch (err) {}
-};
+}
 
-describe('Combined DocumentManager Interface Test', () => {
+let tableName!: string;
+
+describe('db schema', () => {
 
     beforeAll(async () => {
         await testHelper.testBootApp();
 
-        for (const connectionName of connections) {
-            await dropTable(connectionName);
-            await createTable(connectionName);
-        }
+        tableName = TestSchemaModel.getTable()
     });
 
     test('insert test data', async () => {
+
+        await resetTable()
         
         for(const connectionName of connections) {
-            const documentManager = App.container('db')
-                .documentManager(connectionName)
-                .table(tableName);
 
-            const data: Data = {
-                name: 'test'
-            }
+            const insertedDocument = (
+                await queryBuilder(TestSchemaModel, connectionName).insert({
+                    name: 'test'
+                })
+            )?.[0];
 
-            const insertedDocument = await documentManager.insertOne<Data>(data);
             expect(typeof insertedDocument?.id === 'string').toBe(true);
             expect(insertedDocument?.name).toBe('test');
 
         }
     })
 
-    test('alter table', async () => {
+    test('insert test data with altered column', async () => {
+
+        await resetTable()
+        
         for(const connectionName of connections) {
-            const schema = App.container('db').schema(connectionName);
+            const schema = db().schema(connectionName);
 
             if(connectionName === 'mongodb') {
-                expect(true).toBeTruthy();
-                App.container('logger').console('Ignoring MongoDB alter table test');
                 continue;
             }
             
@@ -73,33 +84,12 @@ describe('Combined DocumentManager Interface Test', () => {
                 }
             });
 
-            expect(true).toBeTruthy();
-        }
-    });
-
-    test('insert test data with altered column', async () => {
-        
-        for(const connectionName of connections) {
-            console.log('Connection: ', connectionName);
-            const documentManager = App.container('db')
-                .documentManager(connectionName)
-                .table(tableName);
-
-            const data: Data = {
+            await queryBuilder(TestSchemaModel, connectionName).insert({
                 name: 'test',
                 age: 20
-            }
+            })
 
-            const insertedDocument = await documentManager.insertOne<Data>(data);
-            expect(typeof insertedDocument?.id === 'string').toBe(true);
-            expect(insertedDocument?.name).toBe('test');
-            expect(insertedDocument?.age).toBe(20);
-
-            const foundDocument = await documentManager.findOne<Data>({ 
-                filter: {
-                    id: insertedDocument?.id
-                }
-            });
+            const foundDocument = await queryBuilder(TestSchemaModel, connectionName).where('name', 'test').first();
             expect(typeof foundDocument?.id === 'string').toBe(true);
             expect(foundDocument?.name).toBe('test');
             expect(foundDocument?.age).toBe(20);
@@ -108,13 +98,26 @@ describe('Combined DocumentManager Interface Test', () => {
     })
 
     test('remove column', async () => {
+        await resetTable()
+
         for(const connectionName of connections) {
             const schema = App.container('db').schema<PostgresSchema>(connectionName);
 
             if(connectionName === 'mongodb') {
-                App.container('logger').console('Ignoring MongoDB alter table test');
                 continue;
             }
+            
+            await schema.alterTable(tableName, {
+                addColumn: {
+                    key: 'age',
+                    attribute: DataTypes.INTEGER
+                }
+            });
+
+            await queryBuilder(TestSchemaModel, connectionName).insert({
+                name: 'test',
+                age: 20
+            })
             
             await schema.alterTable(tableName, {
                 removeColumn: {
@@ -122,12 +125,9 @@ describe('Combined DocumentManager Interface Test', () => {
                 }
             });
 
-            const documentManager = App.container('db')
-                .documentManager(connectionName)
-                .table(tableName);
+            const foundDocument = await queryBuilder(TestSchemaModel, connectionName).select(['id', 'name']).where('name', 'test').firstOrFail();
 
-            const foundDocument = await documentManager.findOne<Data>({});
-            expect(typeof foundDocument?.id === 'string').toBe(true);
+            expect(foundDocument?.id).toBeTruthy();
             expect(foundDocument?.name).toBe('test');
             expect(foundDocument?.age).toBeFalsy();
         }
