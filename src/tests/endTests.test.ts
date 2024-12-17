0b77e55469853ca077a4da32fa7127a07b325275
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-12-17 10:41:55 +0000

    endTests fix

--- src/tests/endTests.test.ts
+++ src/tests/endTests.test.ts
@@ -1,20 +1,45 @@
-import { beforeAll, describe, test } from '@jest/globals';
-import { App } from '@src/core/services/App';
+import { describe, test } from '@jest/globals';
+import { logger } from '@src/core/domains/logger/services/LoggerService';
+import PostgresAdapter from '@src/core/domains/postgres/adapters/PostgresAdapter';
+import ParsePostgresConnectionUrl from '@src/core/domains/postgres/helper/ParsePostgresConnectionUrl';
+import { IPostgresConfig } from '@src/core/domains/postgres/interfaces/IPostgresConfig';
 import testHelper from '@src/tests/testHelper';
+import pg from 'pg';
 
-const connections = testHelper.getTestConnectionNames()
 
 describe('end test', () => {
 
-    beforeAll(async () => {
-        await testHelper.testBootApp();
+    test('drop postgres test db', async () => {
+        const dbToDrop = testHelper.getTestDbName()
+        const defaultPostgresCredentials = new PostgresAdapter('', {} as IPostgresConfig).getDefaultCredentials()
+
+        if(!defaultPostgresCredentials) {
+            throw new Error('Invalid default credentials');
+        }
+        
+        const credentials = ParsePostgresConnectionUrl.parse(defaultPostgresCredentials);
+        
+        const client = new pg.Client({
+            user: credentials.username,
+            password: credentials.password,
+            host: credentials.host,
+            port: credentials.port,
+            database: 'postgres'
+        });
+
+        try {
+            await client.connect();
+            await client.query(`DROP DATABASE IF EXISTS "${dbToDrop}"`)
+        }
+        catch (err) {
+            logger().error('Failed to drop test db', err)
+        }
+        finally {
+            await client.end();
+        }    
     })
 
-    test('drop test db', async () => {
-        for(const connectionName of connections) {
-            const schema = App.container('db').schema(connectionName)
-            await schema.dropDatabase(testHelper.getTestDbName());
-        }
-    
-    })
+    // test('drop mongodb test db', async () => {
+    //     throw new Error('Not implemented')
+    // })
 });
