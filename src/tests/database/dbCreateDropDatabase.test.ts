a5bc2695110bcea7d99e9a8e7e8f04d585439191
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-12-17 10:23:25 +0000

    dbCreateDropDatabase fix, deleted postgres client connection test

--- src/tests/database/dbCreateDropDatabase.test.ts
+++ src/tests/database/dbCreateDropDatabase.test.ts
@@ -1,52 +1,53 @@
 /* eslint-disable no-undef */
 import { describe, test } from '@jest/globals';
 import { db } from '@src/core/domains/database/services/Database';
-import PostgresAdapter from '@src/core/domains/postgres/adapters/PostgresAdapter';
-import { App } from '@src/core/services/App';
 import testHelper from '@src/tests/testHelper';
 
 const connections = testHelper.getTestConnectionNames()
+const db_name = 'test_create_drop_db';
+
+/**
+ * Resets databases for all connections by dropping the test database.
+ * Used as a beforeAll hook in database-related tests.
+ */
+const resetDatabases = async () => {
+    for(const connectionName of connections) {
+
+        try {
+            if(await db().schema(connectionName).databaseExists(db_name)) {
+                await db().schema(connectionName).dropDatabase(db_name)
+            }
+        }
+        // eslint-disable-next-line no-unused-vars
+        catch (err) {}
+    }
+}
 
 describe('create and drop a database', () => {
 
     beforeAll(async () => {
         await testHelper.testBootApp()
+        await resetDatabases()
+    })
 
+    afterAll(async () => {
         for(const connectionName of connections) {
-            const schema = App.container('db').schema(connectionName)
-
-            try {
-                await schema.dropDatabase(testHelper.getTestDbName())
-            }
-            // eslint-disable-next-line no-unused-vars
-            catch (err) {}
+            await db().schema(connectionName).dropDatabase(db_name)
         }
     })
 
-    test('test creating db', async () => {
-
+    test('test creating db, then dropping', async () => {
         for(const connectionName of connections) {
             console.log('Connection', connectionName, testHelper.getTestDbName())
+            const schema = db().schema(connectionName)
 
-            const schema = App.container('db').schema(connectionName)
 
-            if(connectionName === 'postgres') {
-                const sequelize = db().getAdapter<PostgresAdapter>().getSequelize()
-                
-                await Promise.all([
-                    sequelize.close(),
-                    sequelize.connectionManager.close()
-                ]);
-            }
-         
-            await schema.createDatabase(testHelper.getTestDbName())
-
-            const exists = await schema.databaseExists(testHelper.getTestDbName())
+            await schema.createDatabase(db_name)
+            const exists = await schema.databaseExists(db_name)
             expect(exists).toBe(true)
 
-            await schema.dropDatabase(testHelper.getTestDbName())
-
-            expect(await schema.databaseExists(testHelper.getTestDbName())).toBe(false)
+            await schema.dropDatabase(db_name)
+            expect(await schema.databaseExists(db_name)).toBe(false)
         }
     })
 });
