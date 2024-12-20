c0dd3b639aeaf34c7cd5b8034b1b4a30f896e2d8
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-12-20 15:38:22 +0000

    Refactor Eloquent tests to utilize queryBuilder with connection support and forEveryConnection utility. Update test setup to reset and repopulate data across multiple connections, enhancing test coverage and consistency in database interactions.

--- src/tests/eloquent/eloquentInsert.test.ts
+++ src/tests/eloquent/eloquentInsert.test.ts
@@ -1,8 +1,8 @@
 /* eslint-disable no-undef */
 import { describe } from '@jest/globals';
-import { app } from '@src/core/services/App';
-import testHelper from '@src/tests/testHelper';
+import { queryBuilder } from '@src/core/domains/eloquent/services/EloquentQueryBuilderService';
 import TestPeopleModel, { resetPeopleTable } from '@src/tests/eloquent/models/TestPeopleModel';
+import testHelper, { forEveryConnection } from '@src/tests/testHelper';
 
 describe('eloquent', () => {
 
@@ -12,32 +12,33 @@ describe('eloquent', () => {
     });
 
     test('test insert records', async () => {
-        const results = await app('query').builder(TestPeopleModel)
-            .insert([
-                {
-                    name: 'John',
-                    age: 25,
-                    createdAt: new Date(),
-                    updatedAt: new Date()
-                },
-                {
-                    name: 'Jane',
-                    age: 30,
-                    createdAt: new Date(),
-                    updatedAt: new Date()
-                }
-            ]);
+        await forEveryConnection(async connection => {
+            const results = await queryBuilder(TestPeopleModel, connection)
+                .insert([
+                    {
+                        name: 'John',
+                        age: 25,
+                        createdAt: new Date(),
+                        updatedAt: new Date()
+                    },
+                    {
+                        name: 'Jane',
+                        age: 30,
+                        createdAt: new Date(),
+                        updatedAt: new Date()
+                    }
+                ]);
 
-        expect(results.count()).toBe(2);
+            expect(results.count()).toBe(2);
 
-        expect(typeof results.get(0)?.id === 'string').toBeTruthy()
-        expect(results.get(0)?.name).toBe('John')
-        expect(results.get(0)?.age).toBe(25)
+            expect(typeof results.get(0)?.id === 'string').toBeTruthy()
+            expect(results.get(0)?.name).toBe('John')
+            expect(results.get(0)?.age).toBe(25)
 
-        expect(typeof results.get(1)?.id === 'string').toBeTruthy()
-        expect(results.get(1)?.name).toBe('Jane')
-        expect(results.get(1)?.age).toBe(30)
-        
+            expect(typeof results.get(1)?.id === 'string').toBeTruthy()
+            expect(results.get(1)?.name).toBe('Jane')
+            expect(results.get(1)?.age).toBe(30)
+        })
 
     });
 });
