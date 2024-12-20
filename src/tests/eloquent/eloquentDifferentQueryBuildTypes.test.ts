9527d84b108ad79b3ff789010f09a91f7e3efa93
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-12-20 14:38:53 +0000

    Refactor Eloquent tests to utilize forEveryConnection for improved connection handling. Update resetAndRepoulateTable to support multiple connections and enhance test coverage across different database connections. This change streamlines the testing process and ensures consistent behavior across various database setups.

--- src/tests/eloquent/eloquentDifferentQueryBuildTypes.test.ts
+++ src/tests/eloquent/eloquentDifferentQueryBuildTypes.test.ts
@@ -2,38 +2,44 @@
 import { describe } from '@jest/globals';
 import { IEloquent } from '@src/core/domains/eloquent/interfaces/IEloquent';
 import { queryBuilder } from '@src/core/domains/eloquent/services/EloquentQueryBuilderService';
-import testHelper from '@src/tests/testHelper';
 import TestPeopleModel, { resetPeopleTable } from '@src/tests/eloquent/models/TestPeopleModel';
+import testHelper, { forEveryConnection } from '@src/tests/testHelper';
 
 const resetAndRepoulateTable = async () => {
     await resetPeopleTable()
 
-    await queryBuilder(TestPeopleModel).insert([
-        {
-            name: 'Alice',
-            age: 25,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        },
-        {
-            name: 'Bob',
-            age: 30,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        },
-        {
-            name: 'John',
-            age: 35,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        },
-        {
-            name: 'Jane',
-            age: 45,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        }
-    ]);
+    await forEveryConnection(async connection => {
+        await queryBuilder(TestPeopleModel, connection).insert([
+            {
+                name: 'Alice',
+                age: 25,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                name: 'Bob',
+                age: 30,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                name: 'John',
+                age: 35,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                name: 'Jane',
+                age: 45,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+        ]);
+    })
+}
+
+const getTestPeopleModelQueryBuilder = (connectionName: string) => {
+    return queryBuilder(TestPeopleModel, connectionName).orderBy('name', 'asc');
 }
 
 describe('eloquent', () => {
@@ -41,122 +47,140 @@ describe('eloquent', () => {
 
     beforeAll(async () => {
         await testHelper.testBootApp()
-        query = queryBuilder(TestPeopleModel).orderBy('name', 'asc');
     });
 
     test('test insert and select', async () => {
         await resetAndRepoulateTable()
 
-        const initialCount = await query.count()
-        expect(initialCount).toBe(4)
+        await forEveryConnection(async connection => {  
+            query = getTestPeopleModelQueryBuilder(connection)
 
-        await query.insert({
-            name: 'Jack',
-            age: 50,
-            createdAt: new Date(),
-            updatedAt: new Date()
+            const initialCount = await query.count()
+            expect(initialCount).toBe(4)
+
+            await query.insert({
+                name: 'Jack',
+                age: 50,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            })
+            const updatedCount = await query.count()
+            expect(updatedCount).toBe(5)
+
+            const results = await query.get()
+            expect(results[0].name).toBe('Alice')
+            expect(results[1].name).toBe('Bob')
+            expect(results[2].name).toBe('Jack')
+            expect(results[3].name).toBe('Jane')
+            expect(results[4].name).toBe('John')
         })
-        const updatedCount = await query.count()
-        expect(updatedCount).toBe(5)
-
-        const results = await query.get()
-        expect(results[0].name).toBe('Alice')
-        expect(results[1].name).toBe('Bob')
-        expect(results[2].name).toBe('Jack')
-        expect(results[3].name).toBe('Jane')
-        expect(results[4].name).toBe('John')
-
     })
 
     test('test insert and delete', async () => {
-        await resetAndRepoulateTable()
-        
-        const initialCount = await query.count()
-        expect(initialCount).toBe(4)
 
-        await query.clone().where('name', 'Bob').delete()
-        const updatedCount = await query.count()
-        expect(updatedCount).toBe(3)
+        await forEveryConnection(async connection => {
+            query = getTestPeopleModelQueryBuilder(connection)
 
-        const results = await query.clone().get()
-        expect(results[0].name).toBe('Alice')
-        expect(results[1].name).toBe('Jane')
-        expect(results[2].name).toBe('John')
+            await resetAndRepoulateTable()
+            
+            const initialCount = await query.count()
+            expect(initialCount).toBe(4)
+
+            await query.clone().where('name', 'Bob').delete()
+            const updatedCount = await query.count()
+            expect(updatedCount).toBe(3)
+
+            const results = await query.clone().get()
+            expect(results[0].name).toBe('Alice')
+            expect(results[1].name).toBe('Jane')
+            expect(results[2].name).toBe('John')
+        })
     })
 
     test('test insert and update', async () => {
         await resetAndRepoulateTable()
         
-        const initialCount = await query.count()
-        expect(initialCount).toBe(4)
+        await forEveryConnection(async connection => {  
+            query = getTestPeopleModelQueryBuilder(connection)
 
-        await query.insert({
-            name: 'Jack',
-            age: 50,
-            createdAt: new Date(),
-            updatedAt: new Date()
+            const initialCount = await query.count()
+            expect(initialCount).toBe(4)
+
+            await query.insert({
+                name: 'Jack',
+                age: 50,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            })
+
+            await query.clone().where('name', 'Jack').update({ age: 51 })
+
+            const updatedCount = await query.count()
+            expect(updatedCount).toBe(5)
+
+            const results = await query.get()
+            expect(results[0].name).toBe('Alice')
+            expect(results[1].name).toBe('Bob')
+            expect(results[2].name).toBe('Jack')
+            expect(results[2].age).toBe(51)
+            expect(results[3].name).toBe('Jane')
+            expect(results[4].name).toBe('John')
         })
-
-        await query.clone().where('name', 'Jack').update({ age: 51 })
-
-        const updatedCount = await query.count()
-        expect(updatedCount).toBe(5)
-
-        const results = await query.get()
-        expect(results[0].name).toBe('Alice')
-        expect(results[1].name).toBe('Bob')
-        expect(results[2].name).toBe('Jack')
-        expect(results[2].age).toBe(51)
-        expect(results[3].name).toBe('Jane')
-        expect(results[4].name).toBe('John')
     })
 
     test('test select and update', async () => {
         await resetAndRepoulateTable()
         
-        const initialCount = await query.count()
-        expect(initialCount).toBe(4)
+        await forEveryConnection(async connection => {
+            query = getTestPeopleModelQueryBuilder(connection)
 
-        await query.insert({
-            name: 'Jack',
-            age: 50,
-            createdAt: new Date(),
-            updatedAt: new Date()
+            const initialCount = await query.count()
+            expect(initialCount).toBe(4)
+
+            await query.insert({
+                name: 'Jack',
+                age: 50,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            })
+
+            await query.clone().where('name', 'Jack').update({ age: 51 })
+
+            const updatedCount = await query.count()
+            expect(updatedCount).toBe(5)
+
+            const jack = await query.clone().where('name', 'Jack').first()
+            expect(jack?.name).toBe('Jack')
+            expect(jack?.age).toBe(51)
         })
-
-        await query.clone().where('name', 'Jack').update({ age: 51 })
-
-        const updatedCount = await query.count()
-        expect(updatedCount).toBe(5)
-
-        const jack = await query.clone().where('name', 'Jack').first()
-        expect(jack?.name).toBe('Jack')
-        expect(jack?.age).toBe(51)
     })
 
     test('test select and delete and insert', async () => {
         await resetAndRepoulateTable()
         
-        const firstBob = await query.clone().where('name', 'Bob').first()
-        console.log('firstBob', firstBob)
-        expect(firstBob?.name).toBe('Bob')
+        await forEveryConnection(async connection => {  
+            query = getTestPeopleModelQueryBuilder(connection)
 
-        await query.clone().where('name', 'Bob').delete()
-        const updatedCount = await query.count()
-        expect(updatedCount).toBe(3)
+            const firstBob = await query.clone().where('name', 'Bob').first()
+            console.log('firstBob', firstBob)
+            expect(firstBob?.name).toBe('Bob')
 
-        const secondBob = await query.clone().where('name', 'Bob').first()
-        expect(secondBob).toBe(null)
+            await query.clone().where('name', 'Bob').delete()
+            const updatedCount = await query.count()
+            expect(updatedCount).toBe(3)
 
-        await query.insert({
-            name: 'Bob',
-            age: 30,
-            createdAt: new Date(),
-            updatedAt: new Date()
+            const secondBob = await query.clone().where('name', 'Bob').first()
+            expect(secondBob).toBe(null)
+
+            await query.insert({
+                name: 'Bob',
+                age: 30,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            })
+
+            const thirdBob = await query.clone().where('name', 'Bob').first()
+            expect(thirdBob?.name).toBe('Bob')
         })
-
-        const thirdBob = await query.clone().where('name', 'Bob').first()
-        expect(thirdBob?.name).toBe('Bob')
     })
-
 });
--- src/tests/eloquent/eloquentFetching.test.ts
+++ src/tests/eloquent/eloquentFetching.test.ts
@@ -1,27 +1,18 @@
 /* eslint-disable no-undef */
 import { describe } from '@jest/globals';
-import Collection from '@src/core/domains/collections/Collection';
-import { IEloquent } from '@src/core/domains/eloquent/interfaces/IEloquent';
+import { queryBuilder } from '@src/core/domains/eloquent/services/EloquentQueryBuilderService';
 import ModelNotFound from '@src/core/exceptions/ModelNotFound';
-import { ModelWithAttributes } from '@src/core/interfaces/IModel';
-import { app } from '@src/core/services/App';
 import { generateUuidV4 } from '@src/core/util/uuid/generateUuidV4';
 import { ITestEmployeeModelData } from '@src/tests/eloquent/models/TestEmployeeModel';
 import TestPeopleModel, { resetPeopleTable } from '@src/tests/eloquent/models/TestPeopleModel';
-import testHelper from '@src/tests/testHelper';
+import testHelper, { forEveryConnection } from '@src/tests/testHelper';
 
 describe('eloquent', () => {
 
-    let query!: IEloquent<TestPeopleModel>;
-    let inserted!: Collection<ModelWithAttributes<TestPeopleModel>>;
-
-    beforeAll(async () => {
-        await testHelper.testBootApp()
+    const resetAndRepopulateTable = async (connection: string) => {
         await resetPeopleTable()
-        
-        query = app('query').builder(TestPeopleModel);
 
-        inserted = await query.clone().insert([
+        return await queryBuilder(TestPeopleModel, connection).clone().insert([
             {
                 name: 'John',
                 age: 25,
@@ -46,127 +37,147 @@ describe('eloquent', () => {
                 createdAt: new Date(),
                 updatedAt: new Date()
             }
-        ]);
+        ])
+    }
+
+    beforeAll(async () => {
+        await testHelper.testBootApp()
     });
 
     test('test find, findOrFail, all and get as model', async () => {
+        await forEveryConnection(async connection => {  
+            const inserted = await resetAndRepopulateTable(connection)
+            const query = queryBuilder(TestPeopleModel, connection)
 
-        const allResults = await query.clone()
-            .all();
+            const allResults = await query.clone()
+                .all();
 
-        console.log(allResults)
+            console.log(allResults)
 
-        expect(allResults.count()).toBe(4);
-        expect(allResults[0] instanceof TestPeopleModel).toBeTruthy();
-        expect(allResults[1] instanceof TestPeopleModel).toBeTruthy();
-        expect(allResults[2] instanceof TestPeopleModel).toBeTruthy();
-        expect(allResults[3] instanceof TestPeopleModel).toBeTruthy();
+            expect(allResults.count()).toBe(4);
+            expect(allResults[0] instanceof TestPeopleModel).toBeTruthy();
+            expect(allResults[1] instanceof TestPeopleModel).toBeTruthy();
+            expect(allResults[2] instanceof TestPeopleModel).toBeTruthy();
+            expect(allResults[3] instanceof TestPeopleModel).toBeTruthy();
 
-        const getResults = await query.clone()
-            .get();
-        expect(getResults.count()).toBe(4);
-        expect(getResults[0] instanceof TestPeopleModel).toBeTruthy();
-        expect(getResults[1] instanceof TestPeopleModel).toBeTruthy();
-        expect(getResults[2] instanceof TestPeopleModel).toBeTruthy();
-        expect(getResults[3] instanceof TestPeopleModel).toBeTruthy();
+            const getResults = await query.clone()
+                .get();
+            expect(getResults.count()).toBe(4);
+            expect(getResults[0] instanceof TestPeopleModel).toBeTruthy();
+            expect(getResults[1] instanceof TestPeopleModel).toBeTruthy();
+            expect(getResults[2] instanceof TestPeopleModel).toBeTruthy();
+            expect(getResults[3] instanceof TestPeopleModel).toBeTruthy();
 
-        const firstResult = await query.clone()
-            .find(inserted[0].id)
-        expect(firstResult instanceof TestPeopleModel).toBeTruthy();
+            const firstResult = await query.clone()
+                .find(inserted[0].id)
+            expect(firstResult instanceof TestPeopleModel).toBeTruthy();
+        })
     })
 
     test('test find, findOrFail, all and get', async () => {
-        
-        expect(inserted.count()).toBe(4);
-        expect(inserted[0].id ?? null).toBeTruthy();
-        expect(inserted[1].id ?? null).toBeTruthy();
-        expect(inserted[2].id ?? null).toBeTruthy();
-        expect(inserted[3].id ?? null).toBeTruthy();
 
-        const allResults = await query.clone().all();
-        expect(allResults.count()).toBe(4);
+        await forEveryConnection(async connection => {  
+            const inserted = await resetAndRepopulateTable(connection)
+            const query = queryBuilder(TestPeopleModel, connection)
 
-        const getResults = await query.clone().get();
-        expect(getResults.count()).toBe(4);
+            expect(inserted.count()).toBe(4);
+            expect(inserted[0].id ?? null).toBeTruthy();
+            expect(inserted[1].id ?? null).toBeTruthy();
+            expect(inserted[2].id ?? null).toBeTruthy();
+            expect(inserted[3].id ?? null).toBeTruthy();
 
-        const firstResult = await query.clone().find(inserted[0].id)
-        expect(firstResult?.id).toBe(inserted[0].id);
+            const allResults = await query.clone().all();
+            expect(allResults.count()).toBe(4);
 
-        const lastResult = await query.clone().find(inserted[3].id)
-        expect(lastResult?.id).toBe(inserted[3].id);
+            const getResults = await query.clone().get();
+            expect(getResults.count()).toBe(4);
 
-        const invalidUuid = generateUuidV4()
-        const invalidResult = await query.clone().find(invalidUuid)
-        expect(invalidResult).toBe(null);
+            const firstResult = await query.clone().find(inserted[0].id)
+            expect(firstResult?.id).toBe(inserted[0].id);
 
-        try {
-            await query.clone().where('id', '=', generateUuidV4()).firstOrFail();
-            expect(false).toBeTruthy();
-        }
-        catch (err) {
-            expect(err).toBeInstanceOf(ModelNotFound)
-        }
+            const lastResult = await query.clone().find(inserted[3].id)
+            expect(lastResult?.id).toBe(inserted[3].id);
 
-        try {
-            await query.clone().where('name', '=', 'Invalid Name').lastOrFail();
-            expect(false).toBeTruthy();
-        }
-        catch (err) {
-            expect(err).toBeInstanceOf(ModelNotFound)
-        }
+            const invalidUuid = generateUuidV4()
+            const invalidResult = await query.clone().find(invalidUuid)
+            expect(invalidResult).toBe(null);
+
+            try {
+                await query.clone().where('id', '=', generateUuidV4()).firstOrFail();
+                expect(false).toBeTruthy();
+            }
+            catch (err) {
+                expect(err).toBeInstanceOf(ModelNotFound)
+            }
+
+            try {
+                await query.clone().where('name', '=', 'Invalid Name').lastOrFail();
+                expect(false).toBeTruthy();
+            }
+            catch (err) {
+                expect(err).toBeInstanceOf(ModelNotFound)
+            }
+        })
     });
 
     test('test selecting fields', async () => {
+        await forEveryConnection(async connection => {  
+            const query = queryBuilder(TestPeopleModel, connection) 
 
-        const resultsWithAllColumns = await query.clone().select('*').get();
+            const resultsWithAllColumns = await query.clone().select('*').get();
 
-        expect(resultsWithAllColumns.count()).toBe(4);
-        expect(resultsWithAllColumns[0].name).toBe('John');
+            expect(resultsWithAllColumns.count()).toBe(4);
+            expect(resultsWithAllColumns[0].name).toBe('John');
 
-        ['id', 'name', 'age', 'createdAt', 'updatedAt'].forEach((column) => {
-            const attribtues = resultsWithAllColumns[0]?.getAttributes() ?? {};
+            ['id', 'name', 'age', 'createdAt', 'updatedAt'].forEach((column) => {
+                const attribtues = resultsWithAllColumns[0]?.getAttributes() ?? {};
 
-            expect(column in attribtues).toBe(true);
+                expect(column in attribtues).toBe(true);
+            })
+
+            const resultsOnlyName = await query.clone().select('name').get();
+            expect(resultsOnlyName.count()).toBe(4);
+            expect(resultsOnlyName[0].name).toBe('John');
+            const resultsOnlyNameAttributes = resultsOnlyName[0]?.getAttributes() ?? {};
+            expect(Object.keys(resultsOnlyNameAttributes)).toHaveLength(1);
         })
-
-        const resultsOnlyName = await query.clone().select('name').get();
-        expect(resultsOnlyName.count()).toBe(4);
-        expect(resultsOnlyName[0].name).toBe('John');
-        const resultsOnlyNameAttributes = resultsOnlyName[0]?.getAttributes() ?? {};
-        expect(Object.keys(resultsOnlyNameAttributes)).toHaveLength(1);
-
-
     });
 
     test('test with raw sql', async () => {
-        
-        const table = query.useTable()
-        const sql = `SELECT * FROM ${table} WHERE name = $1 OR name = $2 ORDER BY name ASC LIMIT 2`;
-        const bindings = ['Alice', 'Bob'];
 
-        const results = await query.clone().raw(sql, bindings);
-        let rows: ITestEmployeeModelData[] = [];
+        await forEveryConnection(async connection => {  
+            const query = queryBuilder(TestPeopleModel, connection)
 
-        if(results &&typeof results === 'object' && 'rows' in results) {
-            rows = results.rows as ITestEmployeeModelData[];
-        }
+            const table = query.useTable()
+            const sql = `SELECT * FROM ${table} WHERE name = $1 OR name = $2 ORDER BY name ASC LIMIT 2`;
+            const bindings = ['Alice', 'Bob'];
 
-        expect(rows.length).toBe(2);
-        expect(rows?.[0].name).toBe('Alice');
-        expect(rows?.[1].name).toBe('Bob');
+            const results = await query.clone().raw(sql, bindings);
+            let rows: ITestEmployeeModelData[] = [];
 
+            if(results &&typeof results === 'object' && 'rows' in results) {
+                rows = results.rows as ITestEmployeeModelData[];
+            }
+
+            expect(rows.length).toBe(2);
+            expect(rows?.[0].name).toBe('Alice');
+            expect(rows?.[1].name).toBe('Bob');
+
+        })
     })
 
     test('test with raw select columns', async () => {
-        
-        const results = await query.clone().selectRaw('"name", "age", "createdAt"').get();
-        expect(results.count()).toBe(4);
+        await forEveryConnection(async connection => {  
+            const query = queryBuilder(TestPeopleModel, connection)
 
-        for(const column of ['name', 'age', 'createdAt']) {
-            const attribtues = results[0]?.getAttributes() ?? {};
-            expect(column in attribtues).toBe(true);
-        }
+            const results = await query.clone().selectRaw('"name", "age", "createdAt"').get();
+            expect(results.count()).toBe(4);
 
+            for(const column of ['name', 'age', 'createdAt']) {
+                const attribtues = results[0]?.getAttributes() ?? {};
+                expect(column in attribtues).toBe(true);
+            }
+        })
     })
 
 });
