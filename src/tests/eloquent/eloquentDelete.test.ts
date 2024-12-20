373f78a12d69e0a7668eb5c90c9ff5bcee547eef
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-12-20 14:14:20 +0000

    Refactor Eloquent tests to utilize queryBuilder with connection support. Update IEloquentQueryBuilderService to accept an optional connectionName parameter in the builder method. This enhances test coverage across multiple database connections and improves connection handling in the Eloquent query builder.

--- src/tests/eloquent/eloquentDelete.test.ts
+++ src/tests/eloquent/eloquentDelete.test.ts
@@ -1,10 +1,9 @@
 /* eslint-disable no-undef */
 import { describe } from '@jest/globals';
-import { IEloquent } from '@src/core/domains/eloquent/interfaces/IEloquent';
 import { queryBuilder } from '@src/core/domains/eloquent/services/EloquentQueryBuilderService';
-import testHelper from '@src/tests/testHelper';
 import TestDepartmentModel, { resetTableDepartmentModel } from '@src/tests/eloquent/models/TestDepartmentModel';
 import TestEmployeeModel, { resetTableEmployeeModel } from '@src/tests/eloquent/models/TestEmployeeModel';
+import testHelper, { forEveryConnection } from '@src/tests/testHelper';
 
 const dateOneYearInPast = new Date();
 dateOneYearInPast.setFullYear(dateOneYearInPast.getFullYear() - 1);
@@ -13,91 +12,93 @@ const resetAndRepopulateTables = async () => {
     await resetTableDepartmentModel()
     await resetTableEmployeeModel()
 
-    const insertedDepartments = await queryBuilder(TestDepartmentModel).insert([
-        {
-            deptName: 'HR',
-            createdAt: new Date(),
-            updatedAt: new Date()
-        },
-        {
-            deptName: 'Sales',
-            createdAt: new Date(),
-            updatedAt: new Date()
-        },
-        {
-            deptName: 'IT',
-            createdAt: new Date(),
-            updatedAt: new Date()
-        },
-        {
-            deptName: 'Finance',
-            createdAt: new Date(),
-            updatedAt: new Date()
-        }
-    ])
+    await forEveryConnection(async connection => {
 
-    await employeeQuery.clone().insert([
-        {
-            deptId: insertedDepartments.find((department) => department?.deptName === 'HR')?.id,
-            name: 'Alice',
-            age: 25,
-            salary: 10000,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        },
-        {
-            deptId: insertedDepartments.find((department) => department?.deptName === 'Sales')?.id,
-            name: 'Bob',
-            salary: 20000,
-            age: 30,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        },
-        {
-            deptId: insertedDepartments.find((department) => department?.deptName === 'IT')?.id,
-            name: 'John',
-            age: 35,
-            salary: 30000,
-            createdAt: new Date(),
-            updatedAt: new Date()
-        },
-        {
-            deptId: insertedDepartments.find((department) => department?.deptName === 'Finance')?.id,
-            name: 'Jane',
-            age: 40,
-            salary: 40000,
-            createdAt: dateOneYearInPast,
-            updatedAt: new Date()
-        },
-        {
-            deptId: null,
-            name: 'NoRelationship',
-            age: 40,
-            salary: 50000,
-            createdAt: dateOneYearInPast,
-            updatedAt: new Date()
-        }
-    ])
+        const insertedDepartments = await queryBuilder(TestDepartmentModel, connection).insert([
+            {
+                deptName: 'HR',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                deptName: 'Sales',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                deptName: 'IT',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                deptName: 'Finance',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+        ])
+    
+        await queryBuilder(TestEmployeeModel, connection).insert([
+            {
+                deptId: insertedDepartments.find((department) => department?.deptName === 'HR')?.id,
+                name: 'Alice',
+                age: 25,
+                salary: 10000,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                deptId: insertedDepartments.find((department) => department?.deptName === 'Sales')?.id,
+                name: 'Bob',
+                salary: 20000,
+                age: 30,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                deptId: insertedDepartments.find((department) => department?.deptName === 'IT')?.id,
+                name: 'John',
+                age: 35,
+                salary: 30000,
+                createdAt: new Date(),
+                updatedAt: new Date()
+            },
+            {
+                deptId: insertedDepartments.find((department) => department?.deptName === 'Finance')?.id,
+                name: 'Jane',
+                age: 40,
+                salary: 40000,
+                createdAt: dateOneYearInPast,
+                updatedAt: new Date()
+            },
+            {
+                deptId: null,
+                name: 'NoRelationship',
+                age: 40,
+                salary: 50000,
+                createdAt: dateOneYearInPast,
+                updatedAt: new Date()
+            }
+        ])
+    })
 }
-
-let employeeQuery!: IEloquent<TestEmployeeModel>;
-
 describe('eloquent', () => {
 
     beforeAll(async () => {
         await testHelper.testBootApp()
-
-        employeeQuery = queryBuilder(TestEmployeeModel)
     });
 
     test('test delete all', async () => {
         await resetAndRepopulateTables();
 
-        await employeeQuery.clone()
-            .delete();
+        await forEveryConnection(async connection => {
+            const employeeQuery = queryBuilder(TestEmployeeModel, connection)
 
-        const resultCount = await employeeQuery.clone().count();
-        expect(resultCount).toBe(0);
+            await employeeQuery.clone()
+                .delete();
+    
+            const resultCount = await employeeQuery.clone().count();
+            expect(resultCount).toBe(0);
+        })
 
     });
 
@@ -105,18 +106,21 @@ describe('eloquent', () => {
 
         await resetAndRepopulateTables();
 
-        await employeeQuery.clone()
-            .where('name', 'Alice')
-            .delete();
+        await forEveryConnection(async connection => {
+            const employeeQuery = queryBuilder(TestEmployeeModel, connection)
 
-        const resultCount = await employeeQuery.clone().count();
-        expect(resultCount).toBe(4);
+            await employeeQuery.clone()
+                .where('name', 'Alice')
+                .delete();
 
-        const resultAlice = await employeeQuery.clone()
-            .where('name', 'Alice')
-            .first();
-        expect(resultAlice).toBeNull();
+            const resultCount = await employeeQuery.clone().count();
+            expect(resultCount).toBe(4);
 
+            const resultAlice = await employeeQuery.clone()
+                .where('name', 'Alice')
+                .first();
+            expect(resultAlice).toBeNull();
+        })
 
     })
 
@@ -124,36 +128,49 @@ describe('eloquent', () => {
 
         await resetAndRepopulateTables();
 
-        await employeeQuery.clone()
-            .where('age', '>', 30)
-            .delete();
+        await forEveryConnection(async connection => {
+            const employeeQuery = queryBuilder(TestEmployeeModel, connection)
 
-        const resultCount = await employeeQuery.clone().count();
-        expect(resultCount).toBe(2);
+            await employeeQuery.clone()
+                .where('age', '>', 30)
+                .delete();
+
+            const resultCount = await employeeQuery.clone().count();
+            expect(resultCount).toBe(2);
+        })
     })
 
     test('delete some by date', async () => {
 
         await resetAndRepopulateTables();
 
-        await employeeQuery.clone()
-            .where('createdAt', '>', dateOneYearInPast)
-            .delete();
+        await forEveryConnection(async connection => {
+            const employeeQuery = queryBuilder(TestEmployeeModel, connection)
 
-        const resultCount = await employeeQuery.clone().count();
-        expect(resultCount).toBe(2);
+            await employeeQuery.clone()
+                .where('createdAt', '>', dateOneYearInPast)
+                .delete();
+
+            const resultCount = await employeeQuery.clone().count();
+            expect(resultCount).toBe(2);
+
+        })
     })
     
     test('delete with limit', async () => {
 
         await resetAndRepopulateTables();
 
-        await employeeQuery.clone()
-            .limit(2)
-            .whereRaw(`id IN (SELECT id FROM tests_employees LIMIT 2)`)
-            .delete();
+        await forEveryConnection(async connection => {
+            const employeeQuery = queryBuilder(TestEmployeeModel, connection)
 
-        const resultCount = await employeeQuery.clone().count();
-        expect(resultCount).toBe(3);
+            await employeeQuery.clone()
+                .limit(2)
+                .whereRaw(`id IN (SELECT id FROM tests_employees LIMIT 2)`)
+                .delete();
+
+            const resultCount = await employeeQuery.clone().count();
+            expect(resultCount).toBe(3);
+        })
     })
 });
