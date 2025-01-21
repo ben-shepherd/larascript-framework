d310839cbb9afd1dc6e38c87fd8e7b71e0d83c30
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-01-21 22:04:15 +0000

    Add repository tests for TestPeopleModel and TestPeopleRepository
    - Introduced comprehensive tests for the Repository class, including findById, findMany, findOne, and a custom method for finding 'Jane'.
    - Implemented a resetAndPopulate function to ensure consistent test data across multiple database connections.
    - Created TestPeopleRepository extending the base Repository to include a specific method for retrieving a record with name 'Jane'.
    - Enhanced test structure and clarity by utilizing forEveryConnection for database operations.

---
+++ src/tests/repositories/TestPeopleRepository.ts
@@ -0,0 +1,20 @@
+import Repository from "@src/core/base/Repository";
+
+import TestPeopleModel from "../eloquent/models/TestPeopleModel";
+export default class TestPeopleRepository extends Repository<TestPeopleModel> {
+
+    constructor(connectionName?: string) {
+        super(TestPeopleModel, connectionName)
+    }       
+
+    /**
+     * Finds one record with name equal to 'Jane'
+     * @returns {Promise<IApiTokenModel | null>}
+     */
+    findOneJane() {
+        return this.findOne({
+            name: 'Jane'
+        })
+    }
+
+}
