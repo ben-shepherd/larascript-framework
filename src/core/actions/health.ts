ec172fb9a98b0eaf6eb238f79ce98aa01d0a1a6f
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-01-23 22:17:12 +0000

    Fix type assertion for MongoDbAdapter in health action

--- src/core/actions/health.ts
+++ src/core/actions/health.ts
@@ -22,7 +22,7 @@ export default async (req: Request, res: Response) => {
 
         // Check if the provider is MongoDB
         if (adapter as unknown instanceof MongoDbAdapter) {
-            const mongoClient = (adapter as MongoDbAdapter).getClient();
+            const mongoClient = (adapter as unknown as MongoDbAdapter).getClient();
             await (mongoClient as MongoClient).db().stats();
         }
         
