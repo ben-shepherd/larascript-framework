b9b462c7d41e6d04b8b8629e2345fa1b3e0f6afb
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-01-26 01:34:24 +0000

    Add request context methods to HttpContext
    - Implement methods for getting and setting request and IP context
    - Add method to retrieve request ID
    - Enhance HttpContext with request context functionality
    - Include JSDoc comments for improved documentation

--- src/core/domains/express/data/HttpContext.ts
+++ src/core/domains/express/data/HttpContext.ts
@@ -1,5 +1,7 @@
-import { NextFunction, Response } from 'express';
 import { BaseRequest } from '@src/core/domains/express/types/BaseRequest.t';
+import { NextFunction, Response } from 'express';
+
+import RequestContext, { requestContext } from '../services/RequestContext';
 
 class HttpContext {
 
@@ -13,14 +15,72 @@ class HttpContext {
     ) {
     }
 
+    /**
+     * Gets the request context.
+     * @returns {RequestContext | undefined} The request context.
+     */
+    public getByRequest<T = unknown>(key: string): T | undefined {
+        return requestContext().getByRequest<T>(this.req, key)
+    }
+
+    /**
+     * Sets the request context.
+     * @param {string} key - The key of the value to set.
+     * @param {unknown} value - The value to set.
+     */
+    public setContext(key: string, value: unknown): void {
+        requestContext().setByRequest(this.req, key, value)
+    }
+
+    /**
+     * Gets the IP context.
+     * @param {string} key - The key of the value to get.
+     * @returns {unknown} The value of the IP context.
+     */
+    public getIpContext<T = unknown>(key: string): T | undefined {
+        return requestContext().getByIpAddress<T>(this.req, key)
+    }
+
+    /**
+     * Sets the IP context.
+     * @param {string} key - The key of the value to set.
+     * @param {unknown} value - The value to set.
+     */
+    public setIpContext(key: string, value: unknown): void {
+        requestContext().setByIpAddress(this.req, key, value)
+    }
+
+    /**
+     * Gets the ID of the current request.
+     * @returns {string} The ID of the current request.
+     */
+    public getId(): string {
+        if(!this.req.id) {
+            throw new Error('Request ID not found');
+        }
+        return this.req.id;
+    }
+
+    /**
+     * Gets the request object.
+     * @returns {BaseRequest} The request object.
+     */
     public getRequest(): BaseRequest {
         return this.req;
     }
 
+    /**
+     * Gets the response object.
+     * @returns {Response} The response object.
+     */
     public getResponse(): Response {
         return this.res;
     }
 
+    /**
+     * Gets the next function.
+     * @returns {NextFunction | undefined} The next function.
+     */
     public getNext(): NextFunction | undefined {
         return this.nextFn;
     }
