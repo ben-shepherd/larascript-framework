3169d19d2bac1c98e19b7fc3836ed470ddbdfa90
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-02-02 00:49:46 +0000

    Simplify routes provider configuration
    - Remove unused imports and variables
    - Streamline route binding by directly accessing auth routes
    - Remove unnecessary comments and empty lines
    - Improve code conciseness in RoutesProvider boot method

--- src/app/providers/RoutesProvider.ts
+++ src/app/providers/RoutesProvider.ts
@@ -1,8 +1,8 @@
 import apiRoutes from "@src/app/routes/api";
 import BaseProvider from "@src/core/base/Provider";
 import healthRoutes from "@src/core/domains/express/routes/healthRoutes";
-import Router from "@src/core/domains/express/routing/Router";
 import { app } from "@src/core/services/App";
+
 
 
 class RoutesProvider extends BaseProvider {
@@ -13,15 +13,11 @@ class RoutesProvider extends BaseProvider {
     public async boot(): Promise<void> {
 
         const expressService = app('express');
-        const authService = app('auth');
-        const authRouter = authService.getAuthRoutes();
-
+        
         // Bind routes
-        expressService.bindRoutes(authRouter ?? new Router())
+        expressService.bindRoutes(app('auth').getAuthRoutes())
         expressService.bindRoutes(healthRoutes);
         expressService.bindRoutes(apiRoutes);
-
-        // Add more routes here
 
     }
 
