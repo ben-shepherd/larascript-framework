7ec5ab9fead25ce340fa8c6406bbbbdf18bdb14e
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-02-11 16:03:11 +0000

    Add SameRule for validating field equality
    - Implement SameRule to validate that one field matches another
    - Support validation for matching field values, handling null/undefined inputs
    - Add comprehensive test coverage for various same field validation scenarios
    - Include custom error message support for field comparison validation

---
+++ src/core/domains/validator/rules/SameRule.ts
@@ -0,0 +1,43 @@
+import AbstractRule from "../abstract/AbstractRule";
+import { IRule, IRuleError } from "../interfaces/IRule";
+
+type TSameOptions = {
+    otherField: string
+}
+
+class SameRule extends AbstractRule<TSameOptions> implements IRule {
+
+    protected name: string = 'same';
+
+    protected errorTemplate: string = 'The :attribute field must match :otherField.';
+
+    constructor(otherField: TSameOptions['otherField']) {
+        super()
+        this.options = {
+            otherField
+        }
+    }
+
+    public async test(): Promise<boolean> {
+        if (this.getData() === undefined || this.getData() === null) {
+            return false;
+        }
+
+        const otherValue = this.getAttributes()?.[this.options.otherField];
+        return this.getData() === otherValue;
+    }
+
+
+    getError(): IRuleError {
+        return {
+            [this.getDotNotationPath()]: [
+                this.formatErrorMessage({
+                    otherField: this.options.otherField
+                })
+            ]
+        }
+    }
+
+}
+
+export default SameRule; 
