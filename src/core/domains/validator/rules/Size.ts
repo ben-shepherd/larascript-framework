e88c319c1a55b7863819bd0147d4f8fe1c972f76
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-02-11 14:21:32 +0000

    Add Size validation rule with comprehensive type support
    - Implement Size rule for validating exact values across numbers, strings, and arrays
    - Support validation for numeric equality, string length, and array item count
    - Add extensive test cases covering various input types and validation scenarios
    - Include custom error messages for different validation contexts

---
+++ src/core/domains/validator/rules/Size.ts
@@ -0,0 +1,88 @@
+import AbstractRule from "../abstract/AbstractRule";
+import { IRule, IRuleError } from "../interfaces/IRule";
+
+type TSizeOptions = {
+    size: number
+}
+
+class Size extends AbstractRule<TSizeOptions> implements IRule {
+
+    protected name: string = 'size'
+
+    protected errorTemplate: string = '';
+
+    protected errorTemplateNumber = 'The :attribute field must be :size.';
+
+    protected errorTemplateString = 'The :attribute field must be :size characters.';
+
+    protected errorTemplateArray = 'The :attribute field must contain :size items.';
+
+    constructor(size: TSizeOptions['size']) {
+        super()
+        this.options = {
+            size
+        }
+    }
+
+    public async test(): Promise<boolean> {
+        this.errorTemplate = this.defaultError
+        
+        if(this.testFailsWhenUndefinedOrNullable()) return false
+
+        if(!this.testNumber()) return false
+        if(!this.testString()) return false
+        if(!this.testArray()) return false
+
+        return true
+    }
+
+    protected testFailsWhenUndefinedOrNullable(): boolean {
+        if(this.getData() === undefined || this.getData() === null) {
+            return true
+        }
+        return false
+    }
+
+    protected testNumber(): boolean {
+        if(typeof this.getData() === 'number') {
+            if(this.getData() as number !== this.options.size) {
+                this.errorTemplate = this.errorTemplateNumber
+                return false
+            }
+        }
+        return true
+    }
+
+    protected testString(): boolean {
+        if(typeof this.getData() === 'string') {
+            if((this.getData() as string).length !== this.options.size) {
+                this.errorTemplate = this.errorTemplateString
+                return false
+            }
+        }
+        return true
+    }
+
+    protected testArray(): boolean {
+        if(Array.isArray(this.getData())) {
+            if((this.getData() as any[]).length !== this.options.size) {
+                this.errorTemplate = this.errorTemplateArray
+                return false
+            }
+        }
+        return true
+    }
+
+    getError(): IRuleError {
+        return {
+            [this.getDotNotationPath()]: [
+                this.formatErrorMessage({
+                    size: this.options.size
+                })
+            ]
+        }
+    }
+
+}
+
+export default Size; 
