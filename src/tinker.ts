45e88f1964fedf739b54f5f618eae513b2230591
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-02-12 22:24:52 +0000

    Introduce TinkerService: Centralize Application Bootstrapping for Development
    - Create TinkerService to manage application initialization
    - Add support for booting with regular or test database configurations
    - Refactor tinker.ts to use new TinkerService
    - Improve environment-specific initialization logic
    - Enhance developer experience for debugging and testing

--- src/tinker.ts
+++ src/tinker.ts
@@ -3,36 +3,24 @@
 import 'dotenv/config';
 import 'tsconfig-paths/register';
 
-import appConfig from '@src/config/app';
-import providers from '@src/config/providers';
-import Kernel from '@src/core/Kernel';
+import TinkerService from '@src/core/domains/tinker/services/TinkerService';
 import { app } from '@src/core/services/App';
-import testHelper from '@src/tests/testHelper';
-
-
-const USE_TEST_DB = false;
-
 (async () => {
 
-    if(USE_TEST_DB) {
-        await testHelper.testBootApp();
-        await app('console').reader(['migrate:fresh', '--seed']).handle();
-    }
-    else {
-        await Kernel.boot({
-            environment: appConfig.env,
-            providers: providers
-        }, {});
-    }
+    // Boot the application
+    // By default, it will boot the regular database
+    // If you want to boot the test database, you can pass `useTestDb: true`
+    await TinkerService.boot({
+        useTestDb: false
+    });
 
+    // Useful services for debugging
     const db        = app('db');
-    const events    = app('events')     
+    const events    = app('events');
+    const logger    = app('logger');
     const query     = app('query');
     const validator = app('validator');
     
-    /**
-    * ADD YOUR CODE HERE
-    */
-
+    // Add your code here
 
 })(); 
