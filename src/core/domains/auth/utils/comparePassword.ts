4ec7e0d02261e2c1772e53546ce692989cfd8d7d
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-02-17 08:57:51 +0000

    Mark comparePassword utility as deprecated in favor of CryptoService

--- src/core/domains/auth/utils/comparePassword.ts
+++ src/core/domains/auth/utils/comparePassword.ts
@@ -6,5 +6,6 @@ import bcrypt from 'bcryptjs'
  * @param password The plain text password
  * @param hashedPassword The hashed password
  * @returns true if the password matches the hashed password, false otherwise
+ * @deprecated Use cryptoService().verifyHash instead
  */
 export default (password: string, hashedPassword: string): boolean => bcrypt.compareSync(password, hashedPassword)
