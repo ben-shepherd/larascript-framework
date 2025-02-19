c026ab04b83658e958841f764e3a2559347d3b0b
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-02-19 23:56:47 +0000

    Add Test App Configuration Key
    - Include 'appKey' in test configuration
    - Enhance testAppConfig with a test-specific application key

--- src/tests/config/testConfig.ts
+++ src/tests/config/testConfig.ts
@@ -4,6 +4,7 @@ import { EnvironmentTesting } from '@src/core/consts/Environment';
 require('dotenv').config();
 
 const testAppConfig: IAppConfig = {
+    appKey: 'test',
     env: EnvironmentTesting
 };
 
