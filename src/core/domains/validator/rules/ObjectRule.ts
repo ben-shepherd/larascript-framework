03b5b01f424782e7191649a05da4ff1e4da96439
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-02-25 13:55:29 +0000

    Refactor ObjectRule to Use Generic Options Type with Required Properties
    - Update ObjectRule to use a generic TObjectOptions type
    - Remove redundant requiredProperties class property
    - Modify validation logic to use options.requiredProperties
    - Improve type safety and consistency in validator rule implementation

--- src/core/domains/validator/rules/ObjectRule.ts
+++ src/core/domains/validator/rules/ObjectRule.ts
@@ -2,15 +2,17 @@
 import AbstractRule from "@src/core/domains/validator/abstract/AbstractRule";
 import { IRule } from "@src/core/domains/validator/interfaces/IRule";
 
-class ObjectRule extends AbstractRule implements IRule {
+type TObjectOptions = {
+    requiredProperties: string[]
+}
+
+class ObjectRule extends AbstractRule<TObjectOptions> implements IRule {
 
     protected name: string = 'object'
 
     protected errorTemplate: string = 'The :attribute field must be an object.';
 
     protected errorPropertiesTemplate = 'The :attribute field must contain the following properties: :properties.'
-
-    protected requiredProperties: string[] = []
 
     constructor(requiredProperties: string[] = []) {
         super({ requiredProperties })
@@ -23,10 +25,11 @@ class ObjectRule extends AbstractRule implements IRule {
         }
 
         const hasRequiredProperties = this.validateRequiredProperties()
+
         if(!hasRequiredProperties) {
 
             this.errorMessage = this.formatErrorMessage({
-                properties: this.requiredProperties.join(', ')
+                properties: this.options.requiredProperties.join(', ')
             }, this.errorPropertiesTemplate)
             
             return false
@@ -37,8 +40,9 @@ class ObjectRule extends AbstractRule implements IRule {
 
     protected validateRequiredProperties(): boolean {
         const data = this.getData() as unknown[]
+        const requiredProperties = this.options.requiredProperties
 
-        for(const property of this.requiredProperties) {
+        for(const property of requiredProperties) {
             if(typeof data[property] === 'undefined') {
                 return false
             }
