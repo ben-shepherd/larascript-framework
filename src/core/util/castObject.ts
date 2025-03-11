1d37eaa5ffdaa9453e2756562e8f779adf623df5
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-03-11 09:03:17 +0000

    Bug fix incorrect import in castObject

--- src/core/util/castObject.ts
+++ src/core/util/castObject.ts
@@ -1,5 +1,6 @@
-import BaseCastable from "@src/core/base/BaseCastable";
 import { TCasts } from "@src/core/domains/cast/interfaces/IHasCastableConcern";
+
+import BaseCastable from "../domains/cast/base/BaseCastable";
 
 const castObject = <ReturnType = unknown>(data: unknown, casts: TCasts): ReturnType => {
     return new BaseCastable().getCastFromObject<ReturnType>(data as Record<string, unknown>, casts)
