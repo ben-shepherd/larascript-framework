08e12bc6d1f7d3ad3c74a5fdc5f4605ac4bfee64
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-05-23 19:36:42 +0100

    Revert ExampleController changes

--- src/app/controllers/ExampleController.ts
+++ src/app/controllers/ExampleController.ts
@@ -1,31 +1,15 @@
 import Controller from "@src/core/domains/http/base/Controller";
 import HttpContext from "@src/core/domains/http/context/HttpContext";
-import { storage } from "@src/core/domains/storage/services/StorageService";
-import { Request } from "express";
 
 class ExampleController extends Controller {
 
-    async fileupload(context: HttpContext) {
-        
-        const req = context.getRequest() as Request;
-        const file = context.getFile('file');
-
-        if (!file) {
-            return this.jsonResponse({
-                message: 'No file uploaded',
-            }, 400)
-        }
-
-        const result = await storage().moveUploadedFile(file)
-
-        return this.jsonResponse({
-            message: 'File uploaded',
-            file: file,
-            result: result.toObject()
+    async example(context: HttpContext) {
+        this.jsonResponse({ 
+            id: context.getId(),
+            message: 'Hello World!',
         }, 200)
-
     }
-
+    
 }
 
 export default ExampleController;
