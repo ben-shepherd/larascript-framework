aeba3a6655d8b0ddfc293e3576e60d1b3b378c98
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-05-27 15:49:09 +0100

    Removed irrelevant import

--- src/app/routes/api.ts
+++ src/app/routes/api.ts
@@ -1,8 +1,6 @@
 import ExampleController from "@src/app/controllers/ExampleController"
 import Route from "@src/core/domains/http/router/Route"
 
-import FileUploadValidator from "../validators/FileUploadValidator"
-
 export default Route.group(router => {
 
     router.get('/example', [ExampleController, 'example'])
