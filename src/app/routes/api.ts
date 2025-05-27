910f934f19844438ed5c5b43120e42b9a3f29eee
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-05-27 11:35:16 +0100

    Revert api.ts changes

--- src/app/routes/api.ts
+++ src/app/routes/api.ts
@@ -1,6 +1,8 @@
 import ExampleController from "@src/app/controllers/ExampleController"
 import Route from "@src/core/domains/http/router/Route"
 
+import FileUploadValidator from "../validators/FileUploadValidator"
+
 export default Route.group(router => {
 
     router.get('/example', [ExampleController, 'example'])
